cc28f7819b45725bc67981bedc8965738a57a33b
Aurore RAMOS <chaplin16@orange.fr>
2021-03-01 20:35:26 +0000

    mise en place des variables avec dotenv

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const app = express();
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const sauce = require('./models/Sauce');
+const Sauce = require('./models/Sauce');
 const user = require('./models/User');
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.jy1jc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.jy1jc.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
 {   useNewUrlParser: true,
     useUnifiedTopology: true })
-        .then(() => console.log('Connexion à MongoDB réussie !'))
-        .catch(() => console.log('Connexion à MongoDB échouée et ZUT!!!'));
+        .then(() => console.log('Connexion à MongoDB réussie!!AU TOP!!!!'))
+        .catch(() => console.log('Connexion à MongoDB échouée et ZUT DE REZUT!!!'));
 
 
 app.use((req, res, next) => {
@@ -24,35 +24,21 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/sauce', (req, res, next) => {
-    delete req.body._id;
-        const sauce =  new Sauce({
-            ...req.body
-        });
+app.post('/api/sauces', (req, res, next) => {
+  delete req.body.id;
+    const sauce =  new Sauce({
+        ...req.body
+    });
     sauce.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré!'}))
-        .catch(error => res.status(400).json({ error: error }));
+    .then(() => res.status(201).json({ message: 'Objet enregistré'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 
-app.use('/api/sauce', (req, res, next) => {
-    const sauce = [
-        {
-            id: 'ObjectId',
-            userId: 'String',
-            name: 'string',
-            manifacturer: 'string',
-            description: 'String',
-            mainPepper: 'string',
-            imageUrl: 'string',
-            heat: 'number',
-            likes: 'number',
-            dislikes: 'number',
-            usersLiked:'[string]',
-            usersDislikes:'[string]'
-        },
-    ];
-    res.status(200).json(sauce);
-  });
+app.use('/api/sauces', (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
