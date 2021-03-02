54c480a3fc8af265895530d5790fa7e5f04bc257
Aurore RAMOS <chaplin16@orange.fr>
2021-03-02 13:18:16 +0000

    creation middleware

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jsonwebtoken = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+      //on trouve le numero du token par son emplacement
+    const token = req.headers.authorization.split(' ')[1];
+    //on le decode
+    const decodedToken = jsonwebtoken.verify(token, 'RANDOM_TOKEN_SECRET');
+    //on en fait un objet JS
+    const userId = decodedToken.userId;
+    //on verifie userId avec celui de la requete
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "Identitée de l'utilisateur non enregistrée";
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('requête non authentifiée!')
+    });
+  }
+};
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
 const sauceControllers = require('../controllers/sauce');
+const auth = require('../middleware/auth');
 
 
-router.post('/', sauceControllers.createSauce); 
-router.put('/:id', sauceControllers.modifySauce);
-router.delete('/:id', sauceControllers.deleteSauce);
-router.get('/:id', sauceControllers.getOneSauce);
-router.get('/', sauceControllers.getAllSauce);
+router.post('/', auth, sauceControllers.createSauce); 
+router.put('/:id', auth, sauceControllers.modifySauce);
+router.delete('/:id', auth, sauceControllers.deleteSauce);
+router.get('/:id', auth, sauceControllers.getOneSauce);
+router.get('/', auth, sauceControllers.getAllSauce);
 router.post('/:id/like', (req, res, next) => {
    //a faire 
 })
