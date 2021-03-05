54be69f0c52799aef5405d90c49920185feb581e
Aurore RAMOS <chaplin16@orange.fr>
2021-03-05 21:21:16 +0000

    regex sur mail et renforcement du MDP

--- models/Sauce.js
+++ models/Sauce.js
@@ -3,59 +3,59 @@ const validator = require('validator');
 
 //modele de sauce
 const sauceSchema = mongoose.Schema({ // on utilise la méthode Schema de Mongoose
-  userId: { 
-    type: String, 
-    required: true 
-  },
-  name: { 
-    type: String, 
-    required: true, 
-    minlength:[5,"pas moins de 5 caratères"], 
-    maxlengt: [30, "pas plus de 30 caractères"], 
-    validate: {
-        validator: function(value) {
-        return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
-        },
-        message: "N'entrez que des lettres et des chiffres, svp"
-    }  
-  },   
-  manufacturer: { 
-    type: String, 
-    minlength:[5,"pas moins de 5 caratères"], 
-    maxlengt: [50, "pas plus de 50 caractères"], 
-    validate: {
-      validator: function(value) {
-      return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
-      },
-      message: "N'entrez que des lettres et des chiffres, svp"
-  } 
-  },
-  description: { 
+  userId: {
     type: String,
-    minlength:[15,"pas moins de 15 caratères"], 
-    maxlengt: [100, "pas plus de 100 caractères"],  
-    validate: {
-      validator: function(value) {
-      return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
-      },
-      message: "N'entrez que des lettres et des chiffres, svp"
-  } 
+    required: true
   },
-  mainPepper: { 
-    type: String, 
-    minlength:[5,"pas moins de 5 caratères"], 
-    maxlengt: [30, "pas plus de 30 caractères"], 
+  name: {
+    type: String,
+    required: true,
+    minlength: [5, "pas moins de 5 caratères"],
+    maxlengt: [30, "pas plus de 30 caractères"],
     validate: {
-      validator: function(value) {
-      return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
+      validator: function (value) {
+        return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
       },
       message: "N'entrez que des lettres et des chiffres, svp"
-  } 
+    }
+  },
+  manufacturer: {
+    type: String,
+    minlength: [5, "pas moins de 5 caratères"],
+    maxlengt: [50, "pas plus de 50 caractères"],
+    validate: {
+      validator: function (value) {
+        return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
+      },
+      message: "N'entrez que des lettres et des chiffres, svp"
+    }
+  },
+  description: {
+    type: String,
+    minlength: [15, "pas moins de 15 caratères"],
+    maxlengt: [100, "pas plus de 100 caractères"],
+    validate: {
+      validator: function (value) {
+        return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
+      },
+      message: "N'entrez que des lettres et des chiffres, svp"
+    }
+  },
+  mainPepper: {
+    type: String,
+    minlength: [5, "pas moins de 5 caratères"],
+    maxlengt: [30, "pas plus de 30 caractères"],
+    validate: {
+      validator: function (value) {
+        return /^[^@&"<>!_$*€£`+=\/;?#]+$/.test(value);
+      },
+      message: "N'entrez que des lettres et des chiffres, svp"
+    }
   },
   heat: { type: Number },
-  imageUrl: { type: String},
+  imageUrl: { type: String },
   likes: { type: Number, default: 0 },
-  dislikes: { type: Number, default: 0},
+  dislikes: { type: Number, default: 0 },
   usersLiked: { type: Array, default: [] },
   usersDisliked: { type: Array, default: [] },
 });
--- models/User.js
+++ models/User.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+ 
+
 const userSchema = mongoose.Schema({
     email: { 
         type: String, 
         required: true, 
-        unique: true
+        unique: true,
+        validate: {
+            validator: function (value) {
+              return /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i.test(value);
+            },
+            message: "Entrez un email valide "
+        }
+        
     },
     password: { 
         type: String, 
-        required: true
+        required: true,
+        minlength: [6, "pas moins de 6 caratères"],
+        validate: {
+            validator: function (value) {
+              return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])$/.test(value);
+            },
+            message: "Entrez un MDP de 6 caractères dont 1maj, 1min et 1chiffre "
+        }
     }
 });
+
+
+
 
 userSchema.plugin(uniqueValidator);
 
