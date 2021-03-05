900a59988c54d81b7904ef03e9a74a59b616e094
Aurore RAMOS <chaplin16@orange.fr>
2021-03-05 17:46:48 +0000

    mise en place regex formulaire

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,18 +1,45 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 //modele de sauce
 const sauceSchema = mongoose.Schema({ // on utilise la méthode Schema de Mongoose
-    userId: { type: String, required: true },
-    name: { type: String, required: true },
-    manufacturer: { type: String },
-    description: { type: String },
-    mainPepper: { type: String },
-    heat: { type: Number },
-    imageUrl: { type: String },
-    likes: { type: Number, default: 0 },
-    dislikes: { type: Number, default: 0},
-    usersLiked: { type: Array, default: [] },
-    usersDisliked: { type: Array, default: [] },
-  });
+  userId: { 
+    type: String, 
+    required: true 
+  },
+  name: { 
+    type: String, 
+    required: true, 
+    minlength:[5,"pas moins de 5 caratères"], 
+    maxlengt: [30, "pas plus de 30 caractères"], 
+    match: [/^[^@&"<>!_$*€£`+=\/;?#]+$/, "pas de caractère spéciaux"],
+  },   
+  manufacturer: { 
+    type: String, 
+    minlength:[5,"pas moins de 5 caratères"], 
+    maxlengt: [50, "pas plus de 50 caractères"], 
+    match: /^[^@&"<>!_$*€£`+=\/;?#]+$/
+  },
+  description: { 
+    type: String,
+    minlength:[15,"pas moins de 15 caratères"], 
+    maxlengt: [100, "pas plus de 100 caractères"],  
+    match: /^[^@"<>!_$*€£`\/?#]+$/ 
+  },
+  mainPepper: { 
+    type: String, 
+    minlength:[5,"pas moins de 5 caratères"], 
+    maxlengt: [30, "pas plus de 30 caractères"], 
+    match: /^[^@"<>!_$*€£`\/?#]+$/ 
+  },
+  heat: { type: Number },
+  imageUrl: { type: String},
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0},
+  usersLiked: { type: Array, default: [] },
+  usersDisliked: { type: Array, default: [] },
+});
+
+
 
 module.exports = mongoose.model('Sauce', sauceSchema);
