c0db3d0dc3e61048a0fbff001a0450e5260f89f3
Aurore RAMOS <chaplin16@orange.fr>
2021-03-06 23:30:50 +0000

    email masqué en BDD

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,12 @@
 const bcrypt = require('bcrypt');
 const jsonwebtoken = require('jsonwebtoken');
-
+const maskData = require('maskdata');
 const User = require('../models/User');
 
 //enregistrement des nouveaux utilisateurs dans BDD
 //regex et hachage du mot de passe
 exports.signup = (req, res, next) => {
+    
     const regexPasseword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/
     if (!regexPasseword.test(req.body.password)) {
         res.status(401).json({ error: "Le mot de passe doit contenir au minimum 6 caractères dont au moins une majuscule, une minuscule, un chiffre et un caractère special!" })
@@ -15,7 +16,7 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
-                email: encryptedMail,
+                email: maskData.maskEmail2(req.body.email),
                 password: hash
             });
             //enregistrement de l utilisateur avec retour promise
@@ -28,13 +29,13 @@ exports.signup = (req, res, next) => {
 
 // connections des utilisateurs deja existants
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    User.findOne({ email: maskData.maskEmail2(req.body.email) })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: "Il n y a pas d'utilisateur avec ce mail!" });
             }
 
-            const regexEmail = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i
+            const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,4}$/;
             if (!regexEmail.test(req.body.email)) {
                 res.status(401).json({ error: "Rentrez un mail valide" })
                 return false
