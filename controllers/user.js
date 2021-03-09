29e5370fac04c9414bf9548cb731a4841b8eb81f
Chaplin16 <chaplin16@orange.fr>
2021-03-09 14:29:34 +0100

    utilisation de password-validator plutot qu une regex pour le mdp

--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,44 @@
 const bcrypt = require('bcrypt');
 const jsonwebtoken = require('jsonwebtoken');
 const maskData = require('maskdata');
+const passwordValidator = require('password-validator');
 const User = require('../models/User');
+
+//creation du schema
+let schema = new passwordValidator();
+schema
+.is().min(8) //au moins 8 caractères
+.is().max(20) // pas plus de 20 caractères
+.has().uppercase() // au moins une minuscule
+.has().lowercase() // au moins une majuscule
+.has().digits(1) // au moins un chiffre
+.has().not().spaces()  //pas d espaces                         
+.is().not().oneOf(['Passw0rd', 'Password123']); // pas de mdp bateau
 
 //enregistrement des nouveaux utilisateurs dans BDD
 //regex et hachage du mot de passe
 exports.signup = (req, res, next) => {
-    
-    const regexPasseword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/
-    if (!regexPasseword.test(req.body.password)) {
-        res.status(401).json({ error: "Le mot de passe doit contenir au minimum 6 caractères dont au moins une majuscule, une minuscule, un chiffre et un caractère special!" })
-        return false
-    }
-
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: maskData.maskEmail2(req.body.email),
-                password: hash
-            });
-            //enregistrement de l utilisateur avec retour promise
-            user.save()
-                .then(() => res.status(201).json({ message: 'Nouvel utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+    if(!schema.validate(req.body.password)) {
+        throw {error:'le mdp doit contenir au moins 8 caractères dont 1chiffre, 1 lettre majuscule et 1 minuscule'}
+    }else {
+        bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+                const user = new User({
+                    email: maskData.maskEmail2(req.body.email),
+                    password: hash
+                });
+                //enregistrement de l utilisateur avec retour promise
+                user.save()
+                    .then(() => res.status(201).json({ message: 'Nouvel utilisateur créé !' }))
+                    .catch(error => res.status(400).json({ error }));
+            })
+            .catch(error => res.status(500).json({ error }));
+        }
 };
 
 // connections des utilisateurs deja existants
 exports.login = (req, res, next) => {
-    User.findOne({ email: maskData.maskEmail2(req.body.email) })
+    User.findOne({ email: maskData.maskEmail2(req.body.email)})
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: "Il n y a pas d'utilisateur avec ce mail!" });
